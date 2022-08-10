5c40f6abcf04d35766d1cd6b40e5dc94079a3183
xbmlz <xbmlz@users.noreply.github.com>
2022-08-10 06:43:05 +0000

    deploy: ee8c15aadafd38c4d66eab9be53479891d4a9e9c

--- assets/index.044bc859.js
+++
@@ -1,1 +0,0 @@
-import{d as e,c as n,o}from"./index.05ae35dc.js";const s=e({__name:"index",setup(a){return(t,r)=>(o(),n("div",null,"dashboard"))}});export{s as default};
---
+++ assets/index.892717c5.js
@@ -0,0 +1,1 @@
+import{d as e,c as n,o}from"./index.a6fdd452.js";const s=e({__name:"index",setup(t){return(a,c)=>(o(),n("div",null,"home"))}});export{s as default};
