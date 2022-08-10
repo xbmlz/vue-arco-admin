8c12d5785c397937b5b5367dadf06be2619d0623
xbmlz <xbmlz@users.noreply.github.com>
2022-08-10 10:53:08 +0000

    deploy: 3f38051c129091b0f3ed7952d8bd6054256d6f34

--- assets/index.892717c5.js
+++
@@ -1,1 +0,0 @@
-import{d as e,c as n,o}from"./index.a6fdd452.js";const s=e({__name:"index",setup(t){return(a,c)=>(o(),n("div",null,"home"))}});export{s as default};
---
+++ assets/index.d9ce2bf7.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,c as n}from"./index.4a2c7fce.js";const o={};function r(t,s){return c(),n("div",null,"Login")}const a=e(o,[["render",r]]);export{a as default};
