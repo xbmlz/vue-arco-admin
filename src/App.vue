e12a7d82bc8f98ae362eb7aea2afa557d8e79fc4
xbmlz <997909544@qq.com>
2023-02-10 14:54:53 +0800

    feat: 添加首屏加载动画

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,3 @@
-<script lang="ts" setup>
-// https://github.com/vueuse/head
-useHead({
-  title: import.meta.env.VITE_APP_TITLE,
-})
-</script>
-
 <template>
   <a-config-provider>
     <router-view />
